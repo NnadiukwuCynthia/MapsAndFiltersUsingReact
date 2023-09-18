71b2396612d540b27d2ded4e4e1e99f0ab95af41
Cynthia Nnadiuwku <cynthiaamaran@gmail.com>
2023-09-18 12:39:35 +0100

    task: Created menuData

---
+++ mappingFilter/Components/menuData.jsx
@@ -0,0 +1,133 @@
+import { Apple} from '../src/Images/Apple.jpg';
+import { Orange} from '../src/Images/Orange.jpg';
+import { Hamburger} from '../src/Images/hamburger.jpg';
+import { PanCake} from '../src/Images/PanCakes.jpg';
+import { VeganPasta} from '../src/Images/VeganPasta.jpg';
+import { Pawpaw} from '../src/Images/Pawpaw.jpg';
+import { Meatball} from '../src/Images/MeatBalls.jpg';
+import { Kiwi} from '../src/Images/Kiwi.jpg';
+import { Spaghetti } from '../src/Images/Spaghetti.jpg';
+import { PorkChops} from '../src/Images/PorkChops.jpg';
+import { Sandwich} from '../src/Images/SandWich.jpg';
+import { Tacos} from '../src/Images/Tacos.jpg';
+import { VeganSoup} from '../src/Images/VeganSoup.jpg';
+import { Toasts } from '../src/Images/ToastedBread.jpg';
+import { Tangerine } from '../src/Images/Tangerine.jpg';
+import { strawberry } from '../src/Images/StrawBerry.jpg'
+
+
+export const menuData = [
+    {
+        Title: 'Apple',
+        Category: 'Fruits',
+        Price: '$5',
+        Image: {Apple},
+        Description: 'Apple is the most popular fruit in the world. It is also known as an apple pie, apple cake, or apple pie.' ,
+    },
+    {
+        Title: 'Orange',
+        Category: 'Fruits',
+        Price: '$10',
+        Image: {Orange},
+        Description: 'Orange is the most popular fruit in the world. It is also known as an orange pie, orange cake, or orange pie.',
+    },
+    {
+        Title: 'Hamburger',
+        Category: 'Food',
+        Price: '$10',
+        Image: {Hamburger},
+        Description: 'Hamburger is the most popular food in the world. It is also known as a hamburger.',
+    },
+    {
+        Title: 'Pancakes',
+        Category: 'Food',
+        Price: '$15',
+        Image: {PanCake},
+        Description: 'Pancakes is the most popular food in the world. It is also known as a pancake.',
+    },
+    {
+        Title: 'Vegan Pasta',
+        Category: 'Food',
+        Price: '$20',
+        Image: {VeganPasta},
+        Description: 'Vegan Pasta is the most popular food in the world. It is also known as a vegan pasta.',
+    },
+    {
+        Title: 'Pawpaw',
+        Category: 'Fruits',
+        Price: '$10',
+        Image: {Pawpaw},
+        Description: 'Pawpaw is the most popular fruit in the world. It is also known as a most generic fruit in the world',
+    },
+    {
+        Title: 'Meatball',
+        Category: 'Food',
+        Price: '$25',
+        Image: {Meatball},
+        Description: 'Meatball is the most popular food in the world. It is also known as a meatball.',
+    },
+    {
+        Title: 'Kiwi',
+        Category: 'Fruits',
+        Price: '$10',
+        Image: {Kiwi},
+        Description: 'Kiwi is the most popular fruit in the world. It is also known as a kiwi fruit.',
+    }, 
+    {
+        Title: 'Spaghetti',
+        Category: 'Food',
+        Price: '$15',
+        Image: {Spaghetti},
+        Description: 'Spaghetti is the most popular food in the world. It is also known as a spaghetti.',
+    },
+    {
+        Title: 'Pork Chops',
+        Category: 'Food',
+        Price: '$15',
+        Image: {PorkChops},
+        Description: 'Pork Chops is the most popular food in the world. It is also known as a pork chops.',
+    },
+    {
+        Title: 'Sandwich',
+        Category: 'Food',
+        Price: '$15',
+        Image: {Sandwich},
+        Description: 'Sandwich is the most popular food in the world. It is also known as a sandwich.',
+    },
+    {
+        Title: 'Tacos',
+        Category: 'Food',
+        Price: '$15',
+        Image: {Tacos},
+        Description: 'Tacos is the most popular food in the world. It is also known as a tacos.',
+    },
+    {
+        Title: 'Vegan Soup',
+        Category: 'Food',
+        Price: '$15',
+        Image: {VeganSoup},
+        Description: 'Vegan Soup is the most popular food in the world. It is also known as a vegan soup.',
+    },
+    {
+        Title: 'Toasted Bread',
+        Category: 'Food',
+        Price: '$15',
+        Image: {Toasts},
+        Description: 'Toasted Bread is the most popular food in the world. It is also known as a toasted bread.',
+    },
+    {
+        Title: 'Tangerine',
+        Category: 'Fruits',
+        Price: '$20',
+        Image: {Tangerine},
+        Description: 'Tangerine is the most popular fruit in the world. It is also known as a tangerine.',
+    },
+    {
+        Title: 'Strawberry',
+        Category: 'Fruits',
+        Price: '$10',
+        Image: {strawberry},
+        Description: 'Strawberry is the most popular fruit in the world. It is also known as a strawberry.',
+    },
+]
+
